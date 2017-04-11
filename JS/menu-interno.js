b86b4c3d08e7fc7d67f7de130a442f1dcdd72099
fxavier12 <faxavier@alunos.utfpr.edu.br>
2017-04-11 16:15:51 -0300

    removidos bugs/icones svg/tabela responsiva

--- JS/menu-interno.js
+++ JS/menu-interno.js
@@ -6,13 +6,14 @@
 * menu interno netflix
 */
 window.onload = function(){//ao carregar a pagina
+						document.getElementById("cancele").style.fill = 'white';
+						document.getElementById("cancele").style.color = 'white';
 						document.getElementById("2").style.display = 'none';
 						document.getElementById("3").style.display = 'none';
 
 				}
 				
 function menu(opcao){
-	console.log("oi");
 	if(opcao == 1){
 		document.getElementById("2").style.display = 'none';
 		document.getElementById("3").style.display = 'none';
@@ -20,13 +21,26 @@ function menu(opcao){
 		document.getElementById('bar-1').className += ' active'; 
 		document.getElementById('bar-2').className = ' barra-menu'; 
 		document.getElementById('bar-3').className = ' barra-menu'; 
+		document.getElementById("cancele").style.fill = 'white';
+		document.getElementById("cancele").style.color = 'white';
+		document.getElementById("device").style.fill = '#757575';
+		document.getElementById("device").style.color = '#757575'; 
+		document.getElementById("planos").style.fill = '#757575';
+		document.getElementById("planos").style.color = '#757575'; 
+
 	}else if(opcao == 2){
 		document.getElementById("1").style.display = 'none';
 		document.getElementById("3").style.display = 'none';
 		document.getElementById('2').style.display = 'block';//show this
 		document.getElementById('bar-2').className += ' active';
 		document.getElementById('bar-1').className = ' barra-menu'; 
-		document.getElementById('bar-3').className = ' barra-menu';  
+		document.getElementById('bar-3').className = ' barra-menu'; 
+		document.getElementById("cancele").style.fill = '#757575';
+		document.getElementById("cancele").style.color = '#757575'; 
+		document.getElementById("planos").style.fill = '#757575';
+		document.getElementById("planos").style.color = '#757575'; 
+		document.getElementById("device").style.fill = 'white';
+		document.getElementById("device").style.color = 'white';
 	}else if(opcao == 3){
 		document.getElementById("2").style.display = 'none';
 		document.getElementById("1").style.display = 'none';
@@ -34,6 +48,12 @@ function menu(opcao){
 		document.getElementById('bar-3').className += ' active'; 
 		document.getElementById('bar-2').className = ' barra-menu'; 
 		document.getElementById('bar-1').className = ' barra-menu'; 
+		document.getElementById("cancele").style.fill = '#757575';
+		document.getElementById("cancele").style.color = '#757575'; 
+		document.getElementById("device").style.fill = '#757575';
+		document.getElementById("device").style.color = '#757575'; 
+		document.getElementById("planos").style.fill = 'white';
+		document.getElementById("planos").style.color = 'white';
 	}
 	
 }
